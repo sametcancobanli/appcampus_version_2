3ddd4e7303a8b181a4f625f96e4e00da7262e5ce
sametcancobanli <samedcancobanli@outlook.com>
2022-12-11 21:31:23 +0300

    sql_files

---
+++ map.sql
@@ -0,0 +1,67 @@
+SELECT * FROM uni_media.string_map;
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'contact', 'Contact Us');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'contact', 'İletişim');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'notification', 'Notification');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'notification', 'Bildirimler');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'like', 'liked your post');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'like', 'gönderini beğendi');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'comment', 'commented on your post');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'comment', 'gönderine yorum yaptı');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'likes', 'others liked your post');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'likes', 'diğerleri gönderini beğendi');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'comments', 'others commented on your post');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'comments', 'diğerleri gönderine yorum yaptı');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'message', 'Message Box');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'message', 'Mesajlar');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'setting', 'Settings');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'setting', 'Ayarlar');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'login', 'Login');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'login', 'Giriş');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'mail', 'E-mail');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'mail', 'E-posta');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'password', 'Password');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'password', 'Şifre');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'remember', 'Remember me');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'remember', 'Beni hatirla');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'forgot', 'Did you forgot your password');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'forgot', 'Şifremi unuttum');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'register', 'Register');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'register', 'Kayit ol');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'name', 'Name');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'name', 'İsim');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'surname', 'Surname');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'surname', 'Soyisim');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'school', 'University');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'school', 'Üniversite');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'department', 'Department');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'department', 'Bölüm');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'year', 'Entry Year');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'year', 'Giriş yili');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'about', 'About');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'about', 'Hakkinda');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'forum', 'Forum');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'forum', 'Forum');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'new_post', 'New post');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'new_post', 'Yeni gönderi');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'write', 'Write something..');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'write', 'Bir şeyler yaz..');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'share', 'Share');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'share', 'Paylaş');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'search', 'Search');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'search', 'Arama');
+
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'profile', 'Profile');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'profile', 'Profil');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'personal', 'Personal info');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'personal', 'Kişisel bilgiler');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'user_post', 'Posts');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'user_post', 'Gönderiler');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('en_US', 'education', 'Education info');
+INSERT INTO `uni_media`.`string_map` (`language_text`, `key_text`, `text`) VALUES ('tr_TR', 'education', 'Eğitim bilgileri');
